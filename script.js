50fbf5669f6006147177fbcd4700348a595ee45d
Andreas Brem <andi.brem@web.de>
2025-09-09 16:46:22 +0200

    project initialize

---
+++ script.js
@@ -0,0 +1,6 @@
+const BASE_URL = 'https://pokeapi.co/api/v2/'; 
+const pokemon = 'pokemon/';
+const pokeSpecies = 'pokemon-species/';
+const pokeEvolution = 'evolution-chain/';
+
+let curOffset = 0;
