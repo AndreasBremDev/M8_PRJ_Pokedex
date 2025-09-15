762c5da2d9b08c3f4ea699679cd2f1200d83a0ec
Andreas Brem <andi.brem@web.de>
2025-09-15 09:27:51 +0200

    updated showModal, styles for modal and updated links for rendering

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
-const typesNameNr = [
-    { normal: 1 }, { fighting: 2 }, { flying: 3 }, { poison: 4 }, { ground: 5 }, { rock: 6 }, { bug: 7 }, { ghost: 8 }, { steel: 9 },
-    { fire: 10 }, { water: 11 }, { grass: 12 }, { electric: 13 }, { psychic: 14 }, { ice: 15 }, { dragon: 16 }, { dark: 17 }, { fairy: 18 }
-]
-let pokeFetchJson = [];
-let pokedex = [];
+const typesNameNr = {
+    normal: 1, fighting: 2, flying: 3, poison: 4, ground: 5, rock: 6, bug: 7, ghost: 8, steel: 9,
+    fire: 10, water: 11, grass: 12, electric: 13, psychic: 14, ice: 15, dragon: 16, dark: 17, fairy: 18
+};
+let dialogRef = document.getElementById('dialog');
 const BASE_URL = 'https://pokeapi.co/api/v2/';
 const pokemon = 'pokemon/';
-const pokeSpecies = 'pokemon-species/';
-const pokeEvolution = 'evolution-chain/';
+// const pokeSpecies = 'pokemon-species/';
+// const pokeEvolution = 'evolution-chain/';
+
+// "next": "https://pokeapi.co/api/v2/pokemon?offset=40&limit=20",
+// "first/current": "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20",
+// "previous": "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20",
+
 let curOffset = 0;
 let limit = 20;
+let pokeFetchJson = [];
+let pokeFetchJsonData2 = []
+let pokedex = [];
+let pokeEvoChainAndText = [];
 
-// let mainCardsRef = document.getElementById('mainCards');
+let spinnerRef = document.getElementById('spinner')
+let mainCardsRef = document.getElementById('mainCards');
 
-function init() {
-    fetchPokemonJson();
+
+
+// Möglichkeit 1: globale Library, alle Daten rein fetch'n
+// Moglichkeit 2: nur fetchen, wenn benötigt wird
+// wenn Modal öffnen, dann Daten g'fetch'd
+
+
+async function init() {
+    toggleLoadingSpinner();
+    await fetchPokemonJson();
     pushFetchToPokedex();
+    toggleLoadingSpinner();
+    renderPokemon();
+
+    
+}
+
+function toggleLoadingSpinner(){
+    document.getElementById('spinner').classList.toggle('d_none');
 }
 
 async function fetchPokemonJson() {
     try {
         for (let i = curOffset; i < limit; i++) {
             let id = i + 1;
-            let pokemonData = await fetch(BASE_URL + pokemon + id);
+            let pokemonData = await fetch(BASE_URL + pokemon + id); // pokemonInformation() {fetch(1, 2, 3)}) -> ACHTUNG Format
             pokeFetchJson.push(await pokemonData.json());
         }
     } catch (err) {
         console.error("message:", err)
     }
+    return pokeFetchJson;
 }
 
 function pushFetchToPokedex() {
@@ -35,27 +61,108 @@ function pushFetchToPokedex() {
         if (pokedex.some(item => item.name === pokeFetchJson.name)) {
             return;
         }
-        pokedex.push({
-            id: pokeFetchJson[i].id,
-            name: pokeFetchJson[i].name,
-            sprites: pokeFetchJson[i].sprites.other["official-artwork"].front_default,
-            abilities: pokeFetchJson[i].abilities,
-            types: pokeFetchJson[i].types,
-            stats: pokeFetchJson[i].stats,
-            weight: pokeFetchJson[i].weight,
-        })
+        pokedex.push(jsonToPokedexData(i))
     }
-    console.log(pokedex);
+    return pokedex;
 }
 
-function renderPokemonWithLimit(pokemonDataAsJson) {
+function jsonToPokedexData(i) {
+    return {
+        id: pokeFetchJson[i].id,
+        name: pokeFetchJson[i].name,
+        sprites: pokeFetchJson[i].sprites.other["official-artwork"].front_default,
+        abilities: pokeFetchJson[i].abilities,
+        types: pokeFetchJson[i].types,
+        stats: pokeFetchJson[i].stats,
+        weight: pokeFetchJson[i].weight,
+    };
+}
+
+function renderPokemon() {
     mainCardsRef.innerHTML = '';
-    for (let i = 0; i < pokemonDataAsJson.results.length; i++) {
-        // console.log(pokemonDataAsJson.results[i])
-        let item = pokemonDataAsJson.results[i];
-        mainCardsRef.innerHTML += getMainCardsHtml(i, item);
+    for (let i = 0; i < pokedex.length; i++) {
+        mainCardsRef.innerHTML += getMainCardsHtml(i);
     }
 }
+
+function renderPokeName(i) {
+    return pokedex[i].name.charAt(0).toUpperCase() + pokedex[i].name.slice(1)
+}
+
+function openDialog(i){
+    dialogRef.showModal();
+    dialogRef.innerHTML = getDialogCardHtml(i);
+    toggleDialogStyling('hidden')
+}
+function closeDialog(){
+    dialogRef.close();
+    toggleDialogStyling('scroll')
+}
+
+function toggleDialogStyling(scrollBehaviour){
+    dialogRef.classList.toggle('opened');
+    document.body.style.overflowY = scrollBehaviour;
+    dialogRef.querySelectorAll('.pokeType').forEach(img => { img.classList.toggle('pokeTypeModal')})
+}
+
+function renderTypeImg(i){
+    // console.log(pokedex[i].types[0].length);
+    let pokeTypeImg = "";
+    for (let k = 0; k < pokedex[i].types.length; k++) {
+        for (const key in typesNameNr) {
+            if (key === pokedex[i].types[k].type.name) {
+                pokeTypeImg += getPokeTypeHtml(key, i, k)
+            }
+        }  
+    }
+    return pokeTypeImg;
+}
+
+
+
 
 // pokeFetchJson[i].types -> [1]...name = nr.
 // const typeIconUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-viii/sword-shield/${typeId}.png`;
+
+
+
+    // await fetchEvoChainAndText();
+    // console.log("pokeFetchJsonData2: ", pokeFetchJsonData2);
+    
+// const fetchArray = [
+//     fetch('https://pokeapi.co/api/v2/pokemon-species/'),
+//     fetch('https://pokeapi.co/api/v2/evolution-chain/'),
+// ]
+
+// async function fetchEvoChainAndText() {
+//     try {
+//         const res = await Promise.allSettled(fetchArray);
+//         const fetchSuccessArray = [];
+//         res.map(obj => {
+//             checkStatusIsFulfilled(obj, fetchSuccessArray);
+//         });
+//         checkLength(fetchSuccessArray);
+
+//         const data = await Promise.all(fetchSuccessArray.map((item) => {
+//             return item.json();
+//         }));
+//         pokeFetchJsonData2.push(data)
+//         // console.log(data);
+        
+//     } catch (err) {
+//         console.error("message:", err)
+//     }
+//     return pokeFetchJsonData2
+// }
+
+// function checkStatusIsFulfilled(obj, fetchSuccessArray) {
+//     if (obj.status === 'fulfilled') {
+//         fetchSuccessArray.push(obj.value);
+//     }
+// }
+
+// function checkLength(fetchSuccessArray) {
+//     if (fetchSuccessArray.length === 0) {
+//         throw new Error("All fetches failes.");
+//     }
+// }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,17 +1,33 @@
-function getMainCardsHtml(i, item) {
+function getMainCardsHtml(i) {
     return `
-        <div class="container">
-            <img src="./assets/img/2_Ivysaur.png" alt="Pokemon Name" class="pokeImg">
+        <div class="container" onclick="openDialog(${i})" data-id="${i + 1}">
+            <img src="${pokedex[i].sprites}" alt="Pokemon ${pokedex[i].name}" class="pokeImg">
             <article class="mainCard">
                 <div class="flex_col">
                     <h3>No. ${curOffset + i + 1}</h3>
-                    <h2>${item.name.charAt(0).toUpperCase() + item.name.slice(1)}</h2>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-viii/sword-shield/12.png"
-                        alt="Pokemon Type" class="pokeType">
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-viii/sword-shield/4.png"
-                        alt="Pokemon Type" class="pokeType">
+                    <h2>${renderPokeName(i)}</h2>
+                    <div id="pokeType">${renderTypeImg(i)}</div>
                 </div>
             </article>
         </div>
     `
+}
+
+function getPokeTypeHtml(key, i, k) {
+    return `<img src="./assets/types/${typesNameNr[key]}.png" 
+    alt="pokemon 
+    type ${pokedex[i].types[k].type.name}" 
+    class="pokeType">`;
+}
+
+function getDialogCardHtml(i) {
+    return `                
+        <img src="${pokedex[i].sprites}" alt="Pokemon ${pokedex[i].name}" class="pokeImg">
+        <article class="mainCard">
+            <div class="flex_col">
+                <h3>No. ${curOffset + i + 1}</h3>
+                <h2>${renderPokeName(i)}</h2>
+                <div id="pokeType">${renderTypeImg(i)}</div>
+            </div>
+        </article>`
 }
