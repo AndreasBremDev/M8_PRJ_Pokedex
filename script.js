6de1bc6a5f349ee3455233f6205fc8f3a375de70
Andreas Brem <andi.brem@web.de>
2025-09-18 10:16:59 +0200

    Updates for fetch and other functions

--- script.js
+++ script.js
@@ -1,86 +1,135 @@
-const typesNameNr = {
+const typesObj = {
     normal: 1, fighting: 2, flying: 3, poison: 4, ground: 5, rock: 6, bug: 7, ghost: 8, steel: 9,
     fire: 10, water: 11, grass: 12, electric: 13, psychic: 14, ice: 15, dragon: 16, dark: 17, fairy: 18
 };
 let dialogRef = document.getElementById('dialog');
 const BASE_URL = 'https://pokeapi.co/api/v2/';
 const pokemon = 'pokemon/';
-// const pokeSpecies = 'pokemon-species/';
+const pokeSpecies = 'pokemon-species/';
+
+let id;
+let curOffset = 0;
+let limit = 20;
+let pokeFetchJson = [];
+let pokeEvoChainFetchJson = []
+let pokedex = [];
+let pokedexEvoChain = [];
+
 // const pokeEvolution = 'evolution-chain/';
 
 // "next": "https://pokeapi.co/api/v2/pokemon?offset=40&limit=20",
 // "first/current": "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20",
 // "previous": "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20",
+// const typeIconUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-viii/sword-shield/${typeId}.png`;
 
-let curOffset = 0;
-let limit = 20;
-let pokeFetchJson = [];
-let pokeFetchJsonData2 = []
-let pokedex = [];
-let pokeEvoChainAndText = [];
 
 let spinnerRef = document.getElementById('spinner')
 let mainCardsRef = document.getElementById('mainCards');
 
 
+async function init(curOffset) {
+    mainCardsRef.innerHTML = '';
+    toggleLoadingSpinner();
+    await fetchPokemonJson(curOffset);
+    pushFetchToPokedex(curOffset);
+    toggleLoadingSpinner();
+    renderPokemon(curOffset);
+
+}
+
+
 
 // Möglichkeit 1: globale Library, alle Daten rein fetch'n
 // Moglichkeit 2: nur fetchen, wenn benötigt wird
-// wenn Modal öffnen, dann Daten g'fetch'd
+//                wenn Modal öffnen, dann Daten g'fetch'd
 
+// ---Prio1--- //
+// fetch Detail for Modal view
+//      save fetch'd data in pokeDetailsFetchJson 
+//      save in -> pokedexDetails
+// save to local storage
+// check, if pokemon @pokedex + @pokedexDetails -> before fetch new
+// ---Prio2--- //
+// load next
+// load prev
+// style next/prev button
+//      cursor:pointer;
+// add next + prev Button in Modal
+//      based of the PokeNumber
 
-async function init() {
-    toggleLoadingSpinner();
-    await fetchPokemonJson();
-    pushFetchToPokedex();
-    toggleLoadingSpinner();
-    renderPokemon();
-
-    
-}
-
-function toggleLoadingSpinner(){
+function toggleLoadingSpinner() {
     document.getElementById('spinner').classList.toggle('d_none');
 }
 
-async function fetchPokemonJson() {
+async function fetchPokemonJson(curOffset) {
     try {
-        for (let i = curOffset; i < limit; i++) {
+        for (let i = curOffset; i < (curOffset + limit); i++) {
             let id = i + 1;
-            let pokemonData = await fetch(BASE_URL + pokemon + id); // pokemonInformation() {fetch(1, 2, 3)}) -> ACHTUNG Format
-            pokeFetchJson.push(await pokemonData.json());
+            let [pokemonRes, speciesRes] = await Promise.all([
+                fetch(BASE_URL + pokemon + id),
+                fetch(BASE_URL + pokeSpecies + id)
+            ]);
+
+            let [pokemonJson, speciesJson] = await Promise.all([
+                pokemonRes.json(),
+                speciesRes.json()
+            ]);
+            pokeFetchJson.push({
+                pokemon: pokemonJson,
+                species: speciesJson
+            });
         }
-    } catch (err) {
-        console.error("message:", err)
+    } catch (error) {
+        console.error("Console.error message:", error)
     }
     return pokeFetchJson;
 }
 
-function pushFetchToPokedex() {
-    for (let i = 0; i < pokeFetchJson.length; i++) {
-        if (pokedex.some(item => item.name === pokeFetchJson.name)) {
-            return;
+function nextSetOfPokemon(nextOffset) {
+    curOffset += nextOffset;
+    init(curOffset)
+}
+// function prevSetOfPokemon(nextOffset){
+//     if (curOffset -= nextOffset <= 0) {
+//         curOffset = 0;
+//         prevButton = document.getElementById('btn_prev').disabled = true;
+//     } else {
+//         curOffset -= nextOffset;
+//     };
+//     init(curOffset)
+// }
+
+function pushFetchToPokedex(curOffset) {
+    for (let i = curOffset; i < pokeFetchJson.length; i++) {
+        let pokeBaseData = pokeFetchJson[i];
+        if (pokedex.some(item => item.name === pokeBaseData.pokemon.name)) {
+            console.log(`Achtung: ${pokeBaseData.pokemon.name} ist schon im Pokedex`);
+            continue;
         }
-        pokedex.push(jsonToPokedexData(i))
+        pokedex.push(jsonToPokedexData(pokeBaseData))
     }
     return pokedex;
 }
 
-function jsonToPokedexData(i) {
+function jsonToPokedexData(pokeBaseData) {
     return {
-        id: pokeFetchJson[i].id,
-        name: pokeFetchJson[i].name,
-        sprites: pokeFetchJson[i].sprites.other["official-artwork"].front_default,
-        abilities: pokeFetchJson[i].abilities,
-        types: pokeFetchJson[i].types,
-        stats: pokeFetchJson[i].stats,
-        weight: pokeFetchJson[i].weight,
+        id: pokeBaseData.pokemon.id,
+        name: pokeBaseData.pokemon.name,
+        sprites: pokeBaseData.pokemon.sprites.other["official-artwork"].front_default,
+        abilities: pokeBaseData.pokemon.abilities,
+        types: pokeBaseData.pokemon.types,
+        stats: pokeBaseData.pokemon.stats,
+        weight: pokeBaseData.pokemon.weight,
+        height: pokeBaseData.pokemon.height,
+        descr_EN: pokeBaseData.species.flavor_text_entries[0].flavor_text.replace(/[\n\f]/g, " "),
+        evoChainLink: pokeBaseData.species.evolution_chain.url,
+        allNames: pokeBaseData.species.names
     };
 }
 
-function renderPokemon() {
-    mainCardsRef.innerHTML = '';
-    for (let i = 0; i < pokedex.length; i++) {
+function renderPokemon(curOffset) {
+    for (let i = curOffset; i < pokedex.length; i++) { // works for +20,
+        // but works NOT for -20
         mainCardsRef.innerHTML += getMainCardsHtml(i);
     }
 }
@@ -89,80 +138,120 @@ function renderPokeName(i) {
     return pokedex[i].name.charAt(0).toUpperCase() + pokedex[i].name.slice(1)
 }
 
-function openDialog(i){
-    dialogRef.showModal();
-    dialogRef.innerHTML = getDialogCardHtml(i);
-    toggleDialogStyling('hidden')
-}
-function closeDialog(){
-    dialogRef.close();
-    toggleDialogStyling('scroll')
+function renderPokeAbilities(i, n) {
+    let abilityName = pokedex[i].abilities[n].ability.name
+    return abilityName.charAt(0).toUpperCase() + abilityName.slice(1)
 }
 
-function toggleDialogStyling(scrollBehaviour){
-    dialogRef.classList.toggle('opened');
-    document.body.style.overflowY = scrollBehaviour;
-    dialogRef.querySelectorAll('.pokeType').forEach(img => { img.classList.toggle('pokeTypeModal')})
-}
-
-function renderTypeImg(i){
-    // console.log(pokedex[i].types[0].length);
+function renderTypeImg(i) {
     let pokeTypeImg = "";
     for (let k = 0; k < pokedex[i].types.length; k++) {
-        for (const key in typesNameNr) {
+        for (const key in typesObj) {
             if (key === pokedex[i].types[k].type.name) {
                 pokeTypeImg += getPokeTypeHtml(key, i, k)
             }
-        }  
+        }
     }
     return pokeTypeImg;
 }
 
+function findNumberOfTypesObj(i) {
+    for (const key in typesObj) {
+        if (key === pokedex[i].types[0].type.name) {
+            return typesObj[key]
+        }
+    }
+}
 
 
+function openDialog(i) {
+    dialogRef.showModal();
+    dialogRef.innerHTML = getDialogCardHtml(i);
+    toggleDialogStyling('hidden');
+}
+function closeDialog() {
+    dialogRef.close();
+    toggleDialogStyling('scroll');
+}
 
-// pokeFetchJson[i].types -> [1]...name = nr.
-// const typeIconUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-viii/sword-shield/${typeId}.png`;
+function toggleDialogStyling(scrollBehaviour) {
+    dialogRef.classList.toggle('opened');
+    document.body.style.overflowY = scrollBehaviour;
+    dialogRef.querySelectorAll('.pokeType').forEach(img => { img.classList.toggle('pokeTypeModal') })
+}
+
+///////
+function saveToLocalStorage() {
+    localStorage.setItem('pokedex', JSON.stringify(pokedex))
+}
+
+function loadFromLocalStorage() {
+    let pokedexLoadFromLocal = JSON.parse(localStorage.getItem('pokedex'))
+    if (pokedexLoadFromLocal !== null) {
+        pokedex = pokedexLoadFromLocal;
+    }
+}
 
 
-
-    // await fetchEvoChainAndText();
-    // console.log("pokeFetchJsonData2: ", pokeFetchJsonData2);
-    
-// const fetchArray = [
-//     fetch('https://pokeapi.co/api/v2/pokemon-species/'),
-//     fetch('https://pokeapi.co/api/v2/evolution-chain/'),
-// ]
-
-// async function fetchEvoChainAndText() {
+// async function fetchPokemonDetailsJson(i) {
 //     try {
-//         const res = await Promise.allSettled(fetchArray);
-//         const fetchSuccessArray = [];
-//         res.map(obj => {
-//             checkStatusIsFulfilled(obj, fetchSuccessArray);
-//         });
-//         checkLength(fetchSuccessArray);
+//         let id = i + 1;
+//         let pokemonDetailData = await fetch(BASE_URL + pokeSpecies + id)
+//         pokeDetailsFetchJson.push(await pokemonDetailData.json());
 
-//         const data = await Promise.all(fetchSuccessArray.map((item) => {
-//             return item.json();
-//         }));
-//         pokeFetchJsonData2.push(data)
-//         // console.log(data);
+//     } catch (error) {
+//         console.error("Console.error fetchPokemonDetailsJson message:", error)
+//     }
+//     return pokeDetailsFetchJson;
+// }
+
+
+
+
+// async function fetchPokemonBaseData(curOffset) {
+//     try {
+//         let allRequests = [];
+
+//         for (let i = 1 + curOffset; i <= 20 + curOffset; i++) {
+//             const pokeFetchArr = [
+//                 fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`),
+//                 fetch(`https://pokeapi.co/api/v2/pokemon-species/${i}/`)
+//             ];
+//             allRequests.push(Promise.allSettled(pokeFetchArr));
+//         }
+
+//         let results = await Promise.all(allRequests);
         
+//         let errors =[];
+
+//         results.forEach((res, index) => {
+//             const id = index + 1 + curOffset;
+
+//             res.forEach((r,idx) => {
+//                 if (r.status === "rejected") {
+//                     errors.push(`Achtung: Fehler bei ID = ${id}, Request# ${idx + 1}: ${r.reason}`);
+//                 }
+//             });
+
+//             const fetchSuccessArray = res
+//                 .filter(obj => obj.status === "fulfilled")
+//                 .map(obj => obj.value);
+
+//             if (fetchSuccessArray.length === 0) {
+//                 console.warn(`Achtung: Keine Daten für ID = ${id}`);
+//                 return;
+//             }
+
+//             Promise.all(fetchSuccessArray.map(item => item.json()))
+//                 .then(data => pokeDetailsFetchJson.push(data))
+//                 .catch(err => console.error(`Fehler beim JSON-Parse für ID=${id}:`, err));
+//         });
+
 //     } catch (err) {
-//         console.error("message:", err)
+//         console.error("message:", err);
 //     }
-//     return pokeFetchJsonData2
+
+//     console.log(pokeDetailsFetchJson);
 // }
 
-// function checkStatusIsFulfilled(obj, fetchSuccessArray) {
-//     if (obj.status === 'fulfilled') {
-//         fetchSuccessArray.push(obj.value);
-//     }
-// }
 
-// function checkLength(fetchSuccessArray) {
-//     if (fetchSuccessArray.length === 0) {
-//         throw new Error("All fetches failes.");
-//     }
-// }
--- scripts/templates.js
+++ scripts/templates.js
@@ -2,9 +2,9 @@ function getMainCardsHtml(i) {
     return `
         <div class="container" onclick="openDialog(${i})" data-id="${i + 1}">
             <img src="${pokedex[i].sprites}" alt="Pokemon ${pokedex[i].name}" class="pokeImg">
-            <article class="mainCard">
+            <article class="mainCard" data-bg="${pokedex[i].types[0].type.name}-${findNumberOfTypesObj(i)}">
                 <div class="flex_col">
-                    <h3>No. ${curOffset + i + 1}</h3>
+                    <h3>No. ${pokedex[i].id}</h3>
                     <h2>${renderPokeName(i)}</h2>
                     <div id="pokeType">${renderTypeImg(i)}</div>
                 </div>
@@ -14,10 +14,7 @@ function getMainCardsHtml(i) {
 }
 
 function getPokeTypeHtml(key, i, k) {
-    return `<img src="./assets/types/${typesNameNr[key]}.png" 
-    alt="pokemon 
-    type ${pokedex[i].types[k].type.name}" 
-    class="pokeType">`;
+    return `<img src="./assets/types/${typesObj[key]}.png" alt="pokemon type ${pokedex[i].types[k].type.name}" class="pokeType">`;
 }
 
 function getDialogCardHtml(i) {
@@ -25,9 +22,71 @@ function getDialogCardHtml(i) {
         <img src="${pokedex[i].sprites}" alt="Pokemon ${pokedex[i].name}" class="pokeImg">
         <article class="mainCard">
             <div class="flex_col">
-                <h3>No. ${curOffset + i + 1}</h3>
+                <h3>No. ${pokedex[i].id}</h3>
                 <h2>${renderPokeName(i)}</h2>
                 <div id="pokeType">${renderTypeImg(i)}</div>
+                <h2>Description</h2>
+                <p>${pokedex[i].descr_EN}</p>
+                <div>
+                    <table>
+                        <tr>
+                            <th width="50%">Height</th>
+                            <th width="50%">Weight</th>
+                        </tr>
+                        <tr>
+                            <td width="50%" class="clipOther">${pokedex[i].height / 10}m</td>
+                            <td width="50%" class="clipOther">${pokedex[i].weight / 10}kg</td>
+                        </tr>
+                    </table>
+                </div> 
+                <div>
+                    <table>
+                        <tr>
+                            <th colspan="2">Abilities</th>
+                        </tr>
+                        <tr>
+                            <td width="50%" class="clipOther">${renderPokeAbilities(i, 0)}</td>
+                            <td width="50%" class="clipOther">${renderPokeAbilities(i, 1)}</td>
+                        </tr>
+                    </table>
+                </div> 
+                <div>
+                    <table>
+                        <tr>
+                            <th colspan="6">Stats</th>
+                        </tr>
+                        <tr>
+                            <th width="16%" title="Health Points">HP</td>
+                            <th width="16%" title="Attack">ATK</td>
+                            <th width="16%" title="Defence">DEF</td>
+                            <th width="16%" title="Special Attack">SpA</td>
+                            <th width="16%" title="Special Defence">SpD</td>
+                            <th width="16%" title="Speed">SPD</td>
+                        </tr>
+                        <tr>
+                            <td width="16%">${pokedex[i].stats[0].base_stat}</td>
+                            <td width="16%">${pokedex[i].stats[1].base_stat}</td>
+                            <td width="16%">${pokedex[i].stats[2].base_stat}</td>
+                            <td width="16%">${pokedex[i].stats[3].base_stat}</td>
+                            <td width="16%">${pokedex[i].stats[4].base_stat}</td>
+                            <td width="16%">${pokedex[i].stats[5].base_stat}</td>
+                        </tr>
+                    </table>
+                </div> 
+                <div>
+                    <table>
+                        <tr>
+                            <th colspan="5">Evolution</th>
+                        </tr>
+                        <tr>
+                            <th width="20%">IMG</th>
+                            <td width="16%">Lv.16</td>
+                            <th width="20%">IMG</th>
+                            <td width="16%">Lv.32</td>
+                            <th width="20%">IMG</th>
+                        </tr>
+                    </table>
+                </div> 
             </div>
         </article>`
 }
